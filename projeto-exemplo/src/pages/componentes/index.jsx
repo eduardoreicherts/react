b34b8186eb5cc956eb09b65b3db20f6d7ba0e1a4
Davi <foreverdavi47@gmail.com>
2023-10-30 21:37:02 -0300

    First Commit

---
+++ projeto-exemplo/src/pages/componentes/index.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+import logo from '../../assets/react.svg'
+
+let Inicial = () => {
+    const [count, setCount] = useState(0)
+    return (
+        <div>
+            <h1>oiiiii</h1>
+            <button onClick = {() => setCount((count) => count + 1)}>click {count}</button>
+        </div>
+    );
+    
+}
+
+export default Inicial;
