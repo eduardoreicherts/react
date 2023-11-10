ba2a8a1f68347d6896cbac879752209a97d657a4
eduardoreicherts <dudureichert86@gmail.com>
2023-11-10 20:25:08 -0300

    .

--- ApiClima/src/pages/wheater.jsx
+++ ApiClima/src/pages/wheater.jsx
@@ -10,7 +10,7 @@ const Weather = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt-br`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=pt_br`;
     const response = await axios.get(url);
     setWeatherData(response.data);
     console.log(url);
@@ -28,7 +28,7 @@ const Weather = () => {
           <form onSubmit={handleSubmit}>
             <input
               type="text"
-              placeholder="Digite a cidade..."
+              placeholder="Digite a cidade"
               onChange={(e) => setCity(e.target.value)}
             />
             <button className="submitBtn" type="submit">Buscar</button>
@@ -36,24 +36,21 @@ const Weather = () => {
         </div>
       )}
       {weatherData !== null && (
-        <div className="weatherCard">
-          <h6>Tempo agora em </h6>
-          <p>{weatherData.name}</p>
+        <div className="mainCard">
+          <div className="weatherCard">
+          <p> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+  <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+</svg> {weatherData.name} - {weatherData.sys.country}</p>
           <img
             src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
-            width="50"
           />
-          <p>Temperatura atual</p>
-          <p>{weatherData.main.temp}°</p>
-          <p>Clima</p>
-          <p>{weatherData.weather[0].description}</p>
-          <p>Sensação térmica</p>
-          <p>{weatherData.main.feels_like}</p>
-          <p>Temperatura mínima</p>
-          <p>{weatherData.main.temp_min}°</p>
-          <p>Temperatura máxima</p>
-          <p>{weatherData.main.temp_max}°</p>
-          <button className="refresh" onClick={refresh}>Voltar</button>
+          <p className="ds">{weatherData.weather[0].description}</p>
+          <h1>{Math.floor(weatherData.main.temp)}°</h1>
+          </div>
+          <p className="max_min">Min: {Math.floor(weatherData.main.temp_min)}°    Max: {Math.floor(weatherData.main.temp_max)}°</p>
+          <button className="refresh" onClick={refresh}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+  <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z"/>
+</svg></button>
         </div>
       )}
     </>
